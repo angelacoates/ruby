097a18084fd7869d23ef5acaf6a0749b183862af
Angela Coates <angela@angelamarii.com>
2017-05-04 09:31:02 -0400

    improved

--- count_pos_sum_neg.rb
+++ count_pos_sum_neg.rb
@@ -1,26 +1,15 @@
+# corrected
 def count_positives_sum_negatives(lst)
-  pos_numbs = []
-  neg_numbs = []
-  array = []
+  pos_numbs=[]
+  neg_numbs=0
 
   lst.each do |numb|
    if numb > 0
      pos_numbs << numb
-    else
-     neg_numbs << numb
+   else
+     neg_numbs += numb
     end
   end
 
-  array << pos_numbs.count
-
-    if neg_numbs.count > 1
-     array << neg_numbs.inject(:+)
-    elsif
-     neg_numbs.empty?
-     array << 0
-    else
-     array << neg_numbs.last
-    end
-
-    return array
-   end
+[pos_numbs.count, neg_numbs]
+end
